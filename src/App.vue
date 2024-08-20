a0ea54151a7633ad18831bc43af581950c0dab90
Mateus Felipe Colla <felipe.colla.m@gmail.com>
2024-08-20 11:02:32 -0300

    Added custom cursor

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 
 .grained
+.cursor
 
 Header
 
@@ -19,6 +20,20 @@ import { onMounted } from 'vue';
 
 onMounted(() => {
     setTimeout(() => titleScroller(`Hey, I'm Mateus Felipe. Lets work together!  `), 2000)
+
+    const cursor = document.querySelector('.cursor');
+
+    document.addEventListener('mousemove', e => {
+        cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+    })
+
+    document.addEventListener('click', () => {
+        cursor.classList.add("expand");
+
+        setTimeout(() => {
+            cursor.classList.remove("expand");
+        }, 500)
+    })
 })
 
 const titleScroller = text => {
